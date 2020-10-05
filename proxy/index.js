37d5cbaf1857568088dc2625d2b112920d0a0ea8
rickboot <rickallen@gmail.com>
2020-10-05 13:15:43 -0700

    Initial commit

---
+++ proxy/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// NEED CORS???
+
+const proxy = createProxyMiddleware({
+  target: 'http://localhost:8083',
+  changeOrigin: true, // for vhosted sites, changes host header to target's host
+  logLevel: 'debug',
+});
+
+const app = express();
+const port = 2112;
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/questions/', proxy);
+
+app.listen(port, () => {
+  console.log(`Proxy running on port ${port}.`);
+});
