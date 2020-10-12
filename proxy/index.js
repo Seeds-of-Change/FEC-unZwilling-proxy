3642d95a286fea724f7472489f4cbcbc0ed87fad
rickboot <rickallen@gmail.com>
2020-10-11 21:39:54 -0700

    Updated to match team member's new IP address

--- proxy/index.js
+++ proxy/index.js
@@ -7,7 +7,7 @@ const port = 80;
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 const Proxy = (targetUrl) => (req, res) => {
-  axios.get(targetUrl + req.originalUrl)
+  axios.get(targetUrl + req.originalUrl + req.paramsId)
     .then((response) => {
       res.send(response.data);
     })
@@ -20,7 +20,7 @@ const proxyQuestions = Proxy('http://54.183.5.42');
 const proxyReviews = Proxy('http://3.131.135.129');
 const proxyFeatureExplorer = Proxy('http://3.101.127.251');
 const proxyRelatedProducts = Proxy('http://18.144.2.219');
-const proxyProductInfo = Proxy('http://3.138.107.143');
+const proxyProductInfo = Proxy('http://3.129.22.244/');
 
 app.use('/questions/:id', proxyQuestions);
 app.use('/review/:id', proxyReviews);
